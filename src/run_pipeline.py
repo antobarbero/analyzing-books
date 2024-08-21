d489515bc73996517a14fbe8ec629d242ccdef12
antobarbero <antobarbero94@gmail.com>
2024-08-21 15:44:34 +0200

    Configuring logs

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -2,21 +2,27 @@ from data_ingestion import ingest_data
 from data_cleaning import clean_data
 from data_transformation import transform_data
 from reporting import visualize_results
+import logging
+
+logging.basicConfig(filename="..\\pipeline.log", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def run_pipeline() -> None:
-    """"""
-    # Data ingestion
-    file_path = ".\\datasets"
+
+    logger.info("Pipeline run starting...")
+
+    file_path = "..\\datasets"
+    logger.info(f"Loading data from {file_path}")
     raw_data = ingest_data(file_path)
 
-    # Data cleaning
+    logger.info("Cleaning data.")
     clean_data_df = clean_data(raw_data)
 
-    # Data transformation
+    logger.info("Analyzing data.")
     transformed_data = transform_data(clean_data_df)
 
-    # Visualization and reporting
+    logger.info("Preparing visualization.")
     visualize_results(transformed_data)
 
 
