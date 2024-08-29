8fe62769e5c2eb4b971fc62d1d6e0f01ff2a6aa9
antobarbero <antobarbero94@gmail.com>
2024-08-29 22:20:56 +0200

    Modify data ingestion to load dataset

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,14 +1,21 @@
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession
+from books_dataset import BooksDataset
 
 
-def ingest_data(file_path: str) -> DataFrame:
+def ingest_data(file_path: str) -> BooksDataset:
     """
     Loads the data from the given `file_path` into a PySpark dataframe.
 
     :param file_path: Path of the file to be loaded.
 
     """
+    # ToDo: settings for file names.
     spark = SparkSession.builder.appName("DataIngestion").getOrCreate()
-    df = spark.read.csv(file_path, header=True, inferSchema=True)
+    book_df = spark.read.csv(
+        file_path + "\\books.csv", header=True, inferSchema=True
+    )  # ToDo: define schema
+    ratings_df = spark.read.csv(
+        file_path + "\\ratings.csv", header=True, inferSchema=True
+    )  # ToDo: define schema
 
-    return df
+    return BooksDataset(books_df=book_df, ratings_df=ratings_df)
