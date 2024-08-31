fdedd8220fb095b4e78e90408e66b914e74b69fd
antobarbero <antobarbero94@gmail.com>
2024-08-31 14:23:42 +0200

    Update run pipeline

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,6 +1,6 @@
 from data_ingestion import ingest_data
 from data_cleaning import clean_data
-from data_transformation import transform_data
+from data_transformation import get_books_popularity_overtime
 from reporting import visualize_results
 import logging
 
@@ -8,19 +8,19 @@ logging.basicConfig(filename="..\\pipeline.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def run_pipeline() -> None:
-
+def run_pipeline(files_path: str = "..\\datasets") -> None:
     logger.info("Pipeline run starting...")
 
-    file_path = "..\\datasets"
-    logger.info(f"Loading data from {file_path}")
-    raw_data = ingest_data(file_path)
+    logger.info(f"Loading data from {files_path}")
+    raw_dataset = ingest_data(files_path)
 
     logger.info("Cleaning data.")
-    clean_data_df = clean_data(raw_data)
+    clean_data_set = clean_data(raw_dataset)
 
     logger.info("Analyzing data.")
-    transformed_data = transform_data(clean_data_df)
+    transformed_data = get_books_popularity_overtime(
+        clean_data_set.books_df, clean_data_set.ratings_df
+    )
 
     logger.info("Preparing visualization.")
     visualize_results(transformed_data)
